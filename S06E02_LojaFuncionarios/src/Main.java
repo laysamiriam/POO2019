29c4ae1c9c8dca0aa667b8514ff3b09c1d9a213d
Laysa Miriam <laysauf@gmail.com>
2019-09-19 10:32:48 -0300

    ArrayList - Funcionario e Loja

--- S06E02_LojaFuncionarios/src/Main.java
+++ S06E02_LojaFuncionarios/src/Main.java
@@ -1,14 +1,42 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Loja topson = new Loja("Loja Topson");
-		topson.adicionarFuncionario("Laysa", "Gerente", 2500);
+		/*topson.adicionarFuncionario("Laysa", "Gerente", 2500);
 		topson.adicionarFuncionario("Guilherme","Vendedor", 998);
 		System.out.println(topson);
 		topson.removerFuncionario("Guilherme");
 		System.out.println(topson);
+		*/
 		
-	}
-
+		Scanner op = new Scanner(System.in);
+		String salariostr= "ui";
+		double salario;
+		
+		
+		while (true) {
+			System.out.println("Você quer: adiconarFuncionario(nome,funcao,salario), removerFuncionario(nome) ou status");
+			String opcao = op.nextLine();
+			String[] vet = opcao.split(" ");
+			
+			if(vet[0].equals("adicionar") || vet[0].equals("adicionarfuncionario")  ) {
+				salariostr = vet[3];
+				salario = Double.parseDouble(salariostr);
+				topson.adicionarFuncionario(vet[1], vet[2],salario);
+				continue;
+			}else if(vet[0].equals("remover") || vet[0].equals("removerfuncionario")) {	
+				topson.removerFuncionario(vet[1]);
+			}else if(vet[0].equals("status")){
+				System.out.println(topson.toString());
+				continue;
+			}else {
+				System.out.println("Não entendi. Tente novamente");
+			}
+		}
+		
+		
+		
+	}	
 }
